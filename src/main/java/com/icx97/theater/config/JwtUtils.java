79df45c0b0bfb5e5b562050ef16f8154064de364
Igor Caran <igor.caran@ddor.rs>
2024-10-22 15:27:03 +0200

    jwt

---
+++ src/main/java/com/icx97/theater/config/JwtUtils.java
@@ -0,0 +1,23 @@
+package com.icx97.theater.config;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtUtils {
+    private String secretKey = "secretKey"; 
+    private long expirationTime = 86400000; // 1 dan u milisekundama
+
+    public String generateToken(Authentication authentication) {
+        return Jwts.builder()
+                .setSubject(authentication.getName())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expirationTime))
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+}
