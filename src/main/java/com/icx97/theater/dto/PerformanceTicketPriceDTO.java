b8b0cc39dc8d75738ab21d32e38edd2dcd8295ac
Igor Caran <igor.caran97@gmail.com>
2025-10-17 21:25:23 +0200

    fix for seats

--- src/main/java/com/icx97/theater/dto/PerformanceTicketPriceDTO.java
+++ src/main/java/com/icx97/theater/dto/PerformanceTicketPriceDTO.java
@@ -1,11 +1,14 @@
 package com.icx97.theater.dto;
 
 import lombok.Data;
+import lombok.Setter;
 
 @Data
 public class PerformanceTicketPriceDTO {
     private Long performanceTicketPriceId;
     private Long performanceId;
     private Long seatTypeId;
+    @Setter
+    private String seatTypeName;
     private Double price;
 }
--- src/main/java/com/icx97/theater/dto/SeatDTO.java
+++ src/main/java/com/icx97/theater/dto/SeatDTO.java
@@ -1,6 +1,7 @@
 package com.icx97.theater.dto;
 
 import lombok.Data;
+import lombok.Setter;
 
 @Data
 public class SeatDTO {
@@ -8,7 +9,9 @@ public class SeatDTO {
     private Long hallId;
     private String seatNumber;
     private Long seatTypeId;
+    @Setter
     private String seatTypeName;
     private Integer rowNum; 
     private Boolean isReserved;
+
 }
